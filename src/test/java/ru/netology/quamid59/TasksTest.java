f852ef15bc3a532f24f93dcffae5f7f0891a6ed2
Zhan Teyvans <118687635+JanTey@users.noreply.github.com>
2023-05-04 15:21:46 +0300

    Update TasksTest.java

--- src/test/java/ru/netology/quamid59/TasksTest.java
+++ src/test/java/ru/netology/quamid59/TasksTest.java
@@ -3,6 +3,8 @@ package ru.netology.quamid59;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TasksTest {
     @Test
     public void testMatchesReturnsFalseWhenQueryNotInSubtasks() {
@@ -10,7 +12,7 @@ public class TasksTest {
         String query = "Subtask 4";
         boolean expected = false;
         boolean actual = epic.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -20,7 +22,7 @@ public class TasksTest {
         String query = "Subtask 2";
         boolean expected = true;
         boolean actual = epic.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -29,7 +31,7 @@ public class TasksTest {
         String query = "task";
         boolean expected = true; // ожидаемый результат, так как строка запроса содержится в названии задачи
         boolean actual = task.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -38,7 +40,7 @@ public class TasksTest {
         String query = "other";
         boolean expected = false; // ожидаемый результат, так как строка запроса не содержится в названии задачи
         boolean actual = task.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -47,7 +49,7 @@ public class TasksTest {
         String query = "topic";
         boolean expected = true; // ожидаемый результат, так как строка запроса содержится в теме встречи
         boolean actual = meeting.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -56,7 +58,7 @@ public class TasksTest {
         String query = "project";
         boolean expected = true; // ожидаемый результат, так как строка запроса содержится в проекте, к которому относится встреча
         boolean actual = meeting.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -65,8 +67,104 @@ public class TasksTest {
         String query = "other";
         boolean expected = false; // ожидаемый результат, так как строка запроса не содержится ни в теме встречи, ни в проекте
         boolean actual = meeting.matches(query);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void testEquals() {
+        Task task1 = new Task(1);
+        Task task2 = new Task(1);
+        Task task3 = new Task(2);
+        Object object = new Object();
+
+        // Сравнение объекта с самим собой
+        assertTrue(task1.equals(task1));
+
+        // Сравнение объекта с null
+        assertFalse(task1.equals(null));
+
+        // Сравнение объекта с объектом другого класса
+        assertFalse(task1.equals(object));
+
+        // Сравнение объекта с объектом того же класса
+        assertTrue(task1.equals(task2));
+        assertFalse(task1.equals(task3));
+    }
+
+    @Test
+    void shouldReturnId() {
+        Task task = new Task(1);
+        assertEquals(1, task.getId());
+    }
+
+    @Test
+    void hashCodeShouldReturnSameValueForEqualTasks() {
+        Task task1 = new Task(1);
+        Task task2 = new Task(1);
+        assertEquals(task1.hashCode(), task2.hashCode());
+    }
+
+    @Test
+    void hashCodeShouldReturnDifferentValueForDifferentTasks() {
+        Task task1 = new Task(1);
+        Task task2 = new Task(2);
+        assertNotEquals(task1.hashCode(), task2.hashCode());
+    }
+
+    @Test
+    void matchesShouldReturnTrueForMatchingQuery() {
+        Task task = new Task(1);
+        assertTrue(task.matches("1"));
+    }
+
+    @Test
+    void matchesShouldReturnFalseForNonMatchingQuery() {
+        Task task = new Task(1);
+        assertFalse(task.matches("2"));
+    }
+
+    @Test
+    public void testGetTitle() {
+        SimpleTask task = new SimpleTask(1, "Task title");
+        assertEquals("Task title", task.getTitle());
+    }
+
+    @Test
+    public void testConstructorWithoutTitle() {
+        SimpleTask task = new SimpleTask(1);
+        assertNull(task.getTitle());
+    }
+
+    @Test
+    public void shouldReturnCorrectTopic() {
+        Meeting meeting = new Meeting(1, "Standup meeting", "Project A", "2023-05-05 09:00:00");
+        assertEquals("Standup meeting", meeting.getTopic());
+    }
+
+    @Test
+    public void shouldReturnCorrectProject() {
+        Meeting meeting = new Meeting(1, "Standup meeting", "Project A", "2023-05-05 09:00:00");
+        assertEquals("Project A", meeting.getProject());
+    }
+
+    @Test
+    public void shouldReturnCorrectStart() {
+        Meeting meeting = new Meeting(1, "Standup meeting", "Project A", "2023-05-05 09:00:00");
+        assertEquals("2023-05-05 09:00:00", meeting.getStart());
+    }
+
+    @Test
+    void shouldReturnSubtasks() {
+        String[] subtasks = {"Subtask 1", "Subtask 2", "Subtask 3"};
+        Epic epic = new Epic(1, subtasks);
+        assertArrayEquals(subtasks, epic.getSubtasks());
+    }
+
+    @Test
+    void shouldReturnEmptySubtasks() {
+        String[] subtasks = {};
+        Epic epic = new Epic(1, subtasks);
+        assertArrayEquals(subtasks, epic.getSubtasks());
+    }
 
 }
